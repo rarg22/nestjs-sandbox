3c3bf163169fd9d792845fe19e29c6eaf8ff68ee
Raul Argueta <rargue22@gmail.com>
2020-07-12 14:16:12 -0600

    fixing persons controller response

--- src/persons/persons.controller.ts
+++ src/persons/persons.controller.ts
@@ -5,30 +5,30 @@ export class PersonsController {
 
     @Get()
     getAll(@Res() res) {
-        res.json({
+        res.json([{
             key: 1,
             name: 'Bob',
             age: 30
         },
-            {
-                key: 2,
-                name: 'Joe',
-                age: 12
-            },
-            {
-                key: 3,
-                name: 'Maria',
-                age: 23
-            },
-            {
-                key: 4,
-                name: 'Ed',
-                age: 23
-            },
-            {
-                key: 5,
-                name: 'Juan',
-                age: 23
-            })
+        {
+            key: 2,
+            name: 'Joe',
+            age: 12
+        },
+        {
+            key: 3,
+            name: 'Maria',
+            age: 23
+        },
+        {
+            key: 4,
+            name: 'Ed',
+            age: 23
+        },
+        {
+            key: 5,
+            name: 'Juan',
+            age: 23
+        }])
     }
 }
