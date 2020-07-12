925aaa736943cd290d1a8064bb2da69e6cb86028
Raul Argueta <rargue22@gmail.com>
2020-07-12 14:06:12 -0600

    adding persons controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { CatsController } from './cats/cats.controller';
 import { AppController } from './app.controller';
+import { PersonsController } from './persons/persons.controller';
 
 @Module({
   imports: [],
-  controllers: [CatsController, AppController],
+  controllers: [CatsController, AppController, PersonsController],
   providers: [],
 })
 export class AppModule { }
